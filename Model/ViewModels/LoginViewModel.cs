12621937225a3c5695aa4010c5a17678762f97d8
ducth <ducth@DUCTHANG>
2017-01-25 16:15:24 +0700

    update Login

--- Model/ViewModels/LoginViewModel.cs
+++ Model/ViewModels/LoginViewModel.cs
@@ -1,8 +1,12 @@
-﻿namespace Model.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model.ViewModels
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Hãy nhập tên tài khoản")]
         public string UserName { get; set; }
+        [Required(ErrorMessage = "Hãy nhập mật khẩu")]
         public string Password { get; set; }
         public bool RememberMe { get; set; }
     }
--- Web/Controllers/AccountController.cs
+++ Web/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using Model.ViewModels;
 using Service.Interfaces;
 using Service.Serivices;
@@ -40,10 +41,26 @@ namespace Web.Controllers
                 var user = _userService.GetUser(loginViewModel.UserName, loginViewModel.Password);
                 if (user != null)
                 {
-                    return Content("Ok baby");
+                    FormsAuthentication.SetAuthCookie(loginViewModel.UserName, loginViewModel.RememberMe);
+                    return RedirectToAction("Demo");
                 }
-                
+                ModelState.AddModelError("","Sai tai khoan hoac mat khau");
             }
+            return View(loginViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult LogOut()
+        {
+            //WebSecurity.Logout();
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account");
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult Demo()
+        {
             return View();
         }
         #endregion
